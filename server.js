85c2583c65c23a7a3ae53d6fee8f032ae9bcafa5
Buchi <laganch@yahoo.com>
2023-02-21 17:58:21 +0100

    update

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/government-frontend'));
+app.use(express.static('./dist/government_frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/government-frontend/'}),
+    res.sendFile('index.html', {root: 'dist/government_frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
